e531506f09d710663dcae7e1a210f11e36188044
Carlos S <84247106+CarlosDev314159@users.noreply.github.com>
2023-01-06 05:55:26 -0300

    Update pip_installer.py

--- pip_installer.py
+++ pip_installer.py
@@ -2,6 +2,7 @@ import subprocess
 
 # Execute esse arquivo para instalar as dependências de forma  automática
 try:
+    subprocess.call(["sudo", "apt", "install", "python3-tk"])
     subprocess.call(["pip", "install", "-r", "requirements.txt"])
 except Exception as e:
     print(f"Error log: {e}")
